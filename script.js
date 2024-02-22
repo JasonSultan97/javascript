778c441796b847f84d6652269279389cc4d2f5d8
JasonSultan97 <jasonsulton97@gmail.com>
2024-02-22 17:33:57 +0900

    homework

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 
+// var num = 4;
 
-let num1 = 23
-let num2 = 34
+// num % 2 === 0 && console.log('Juft son');
 
-let sum = num1+num2
+// var num = 5;
 
-console.log(sum);
+// num % 2 !== 0 && console.log('Toq son');
+
+
+////////2-task/////////
+
+// let ball = 99;
+// ball >= 86 && ball <= 100 && console.log('A');
+// ball >= 61 && ball <= 85 && console.log('B');
+// ball>=50&& ball <= 60 && console.log('C');
+// ball < 50 && console. log( 'Failed');
+
+// /////////3-task/////////
+
+// var meva = 'olma'; 
+// meva === 'olma' && console.log(10000); 
+// var meva = 'nok'; 
+// meva === 'nok' && console.log(12000); 
+// var meva = 'tarvuz'; 
+// meva === 'tarvuz' && console.log(20000); 
+// var meva = 'pomidor'; 
+// meva !== 'olma' && console.log('Bunday meva yuq!');
+
