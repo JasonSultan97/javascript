2cb36f042acb6c749761d64b347e8d6c42c2254e
JasonSultan97 <jasonsulton97@gmail.com>
2024-02-22 18:06:12 +0900

    if else turnery task 3,4,5 added

--- hwork1.js
+++ hwork1.js
@@ -16,16 +16,56 @@
 
 ////////////////2-task////////////////////
 
-let fruit = 'olma';
+// let fruit = 'olma';
 
 
-if(fruit === 'olma') {
-    console.log(10000);
-}else if(fruit === 'nok') {
-    console.log(12000);
-}else if(fruit === 'tarvuz') {
-    console.log(20000);
-}else {
-    console.log('Bunday meva yoq');
-}
+// if(fruit === 'olma') {
+//     console.log(10000);
+// }else if(fruit === 'nok') {
+//     console.log(12000);
+// }else if(fruit === 'tarvuz') {
+//     console.log(20000);
+// }else {
+//     console.log('Bunday meva yoq');
+// }
+
+
+////////////////3-task////////////////////
+
+// var num = 6; //10; //30
+
+
+// if(num === 6) {
+//     console.log('3ga bolinadi');
+// }else if(num === 10) {
+//     console.log('5ga bolinadi');
+// }else {
+//     console.log('3ga va 5ga bolinadi');
+// }
+
+//////////////////4-task//////////////////////
+
+// let browser = 'Edge';
+
+
+// if (browser === 'Edge') {
+//     console.log("You've got the Edge!");
+// } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+//     console.log('Okay we support these browsers too');
+// } else {
+//     console.log('We hope that this page looks ok!');
+// }
+
+////////////////5-task//////////////////////
+
+// let login = '';
+
+
+// let message = (login == 'Employee') ? 'Hello' : 
+//               (login === 'director') ? 'Greetings' : 
+//               (login === '') ? 'No login' : 
+//               '';
+
+// console.log(message);
+
 
