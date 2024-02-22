9c0c000d021d461bf3f167c426e2d1bdb7fe6c38
JasonSultan97 <jasonsulton97@gmail.com>
2024-02-22 18:05:15 +0900

    if else task-2 added

--- hwork1.js
+++ hwork1.js
@@ -1,15 +1,31 @@
 //////////////1-task///////////////
 
-var result = 100;
+// var result = 100;
 
-if (result >= 86) {
-    console.log('A');
-} else if (result >= 61 && result <= 85) {
-    console.log('B');
-} else if (result >= 46 && result <= 60) {
-    console.log('C');
-} else {
-    console.log('Failed');
+
+// if (result >= 86) {
+//     console.log('A');
+// } else if (result >= 61 && result <= 85) {
+//     console.log('B');
+// } else if (result >= 46 && result <= 60) {
+//     console.log('C');
+// } else {
+//     console.log('Failed');
+// }
+
+
+////////////////2-task////////////////////
+
+let fruit = 'olma';
+
+
+if(fruit === 'olma') {
+    console.log(10000);
+}else if(fruit === 'nok') {
+    console.log(12000);
+}else if(fruit === 'tarvuz') {
+    console.log(20000);
+}else {
+    console.log('Bunday meva yoq');
 }
-
 
